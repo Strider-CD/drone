6b62b31ad01f1e10e192f2cf692dc7b92660c694
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2015-10-02 21:12:11 +0200

    added a test placeholder

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict'
+// post on testing hapi https://medium.com/the-spumko-suite/testing-hapi-services-with-lab-96ac463c490a
+
+require('babel/register')
+
+var tape = require('tape')
+
+tape('placeholder - add meaningful tests', function (t) {
+  t.ok(parseFloat('1') === 1, 'No meaningful tests present... Please add some')
+  t.end()
+})
