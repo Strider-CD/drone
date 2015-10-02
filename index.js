939f12f20b9af8cbc4b46a1fd86980095b7d9647
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2015-10-02 18:03:59 +0200

    cleanup + get rid of standard errors

--- index.js
+++ index.js
@@ -1,58 +1,34 @@
 'use strict'
 
 var Primus = require('primus')
-var Rooms = require('primus-rooms')
 var config = require('config')
-var Wreck = require('wreck')
 var workflowHandler = require('./lib/eventHandlers/workflow.js')
 var jobSourceCorePoller = require('./lib/jobSources/corePoller.js')
-
 var EventEmitter = require('eventemitter3')
-var spawn = require('child_process').spawn
-var StreamSplitter = require('stream-splitter')
-var tmp = require('tmp')
-var yaml = require('js-yaml')
-var fs = require('fs')
-require('shelljs/global')
 
 var pollingInterval = 1000
-var token = null
 
-if(!(config.coreAPIToken)) {
-  if(config.coreAPIToken.length === 0) {
-    process.exit(1) 
+if (!(config.coreAPIToken)) {
+  if (config.coreAPIToken.length === 0) {
+    process.exit(1)
   }
 }
 
-var socket = new Primus.createSocket()
+var socket = new Primus.createSocket() // eslint-disable-line new-cap
 var client = socket(config.coreUrl + '?token=' + config.coreAPIToken)
-
-var jobRunnerStatus = 'done'
 
 var emitter = new EventEmitter()
 
-var startDir = new workflowHandler(emitter)
+workflowHandler(emitter)
 jobSourceCorePoller(emitter, pollingInterval)
 
 var plugins = require('./lib/plugins')
 plugins(emitter, client)
 
-var basicHeader = function (username, password) {
-  return 'Basic ' + (new Buffer(username + ':' + password, 'utf8')).toString('base64')
-}
-
 console.log('using token', config.coreAPIToken)
 
-var options = {
-  headers: {Authorization: config.coreAPItoken},
-  timeout: 1000, // 1 second, default: unlimited
-  maxBytes: 1048576, // 1 MB, default: unlimited
-}
-
 // TODO: periodic token refresh (in corePoller)?
-
 emitter.emit('auth.token', config.coreAPIToken)
-
 
 if (!module.parent) {
 }
--- util/createDroneUser.js
+++ util/createDroneUser.js
@@ -6,17 +6,17 @@ var basicHeader = function (username, password) {
 }
 
 var options = {
-    headers:   {authorization: basicHeader(process.env.CORE_ADMIN_USER, process.env.CORE_ADMIN_PWD)},
-    timeout:   1000,    // 1 second, default: unlimited
-    maxBytes:  1048576, // 1 MB, default: unlimited
+    headers: {authorization: basicHeader(process.env.CORE_ADMIN_USER, process.env.CORE_ADMIN_PWD)},
+    timeout: 1000,    // 1 second, default: unlimited
+    maxBytes: 1048576 // 1 MB, default: unlimited
 }
 
 var adminToken = null
 
-var req = Wreck.request('GET', config.coreUrl + '/api/v1/users/login', options, function (err, res) {
+Wreck.request('GET', config.coreUrl + '/api/v1/users/login', options, function (err, res) {
   if (err) return console.error('Authentication error: ', err)
-  if(res.statusCode !== 200) return console.error('Authentication error')
-  if(!(res.headers.authorization)) return console.error('Authentication error')
+  if (res.statusCode !== 200) return console.error('Authentication error')
+  if (!(res.headers.authorization)) return console.error('Authentication error')
   adminToken = res.headers.authorization
 
   var drone = {
@@ -24,15 +24,15 @@ var req = Wreck.request('GET', config.coreUrl + '/api/v1/users/login', options, 
   }
 
   var options = {
-      headers:   {authorization: adminToken},
-      payload:   JSON.stringify(drone),
-      timeout:   1000,    // 1 second, default: unlimited
-      maxBytes:  1048576, // 1 MB, default: unlimited
+      headers: {authorization: adminToken},
+      payload: JSON.stringify(drone),
+      timeout: 1000,    // 1 second, default: unlimited
+      maxBytes: 1048576 // 1 MB, default: unlimited
   }
 
-  var req = Wreck.request('POST', config.coreUrl + '/api/v1/drones', options, function (err, res) {
+  Wreck.request('POST', config.coreUrl + '/api/v1/drones', options, function (err, res) {
     if (err) return console.error('Create drone failed: ', err)
-    if(res.statusCode !== 200) return console.error('Create drone failed status != 200')
+    if (res.statusCode !== 200) return console.error('Create drone failed status != 200')
     console.log('drone username / password should be registered')
     console.log('Your API key is:')
     console.log(res.headers.authorization)
